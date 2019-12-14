fdf29e4e79b48746a6129a5ca5a56ab3ee25c1e1
tdofat <dofat.tony@gmail.com>
2019-12-13 20:20:08 -0500

    started menu

--- gitCloneMe.sh
+++ gitCloneMe.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env bash
 
 # Get User Input (clone ssh)
+echo "-------------------->"
 echo Welcome to CloneGitMe
+echo "<--------------------"
 read -p 'Enter Git SSH Key: ' cloneAdd
 
 if [[ '$cloneAdd' =~ ^[a-z]+@[a-z]+\.[a-z]\:[A-Za-z0-9._]+\/[A-za-z0-9._-]+\.[a-z] ]]
