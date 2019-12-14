2277d639601e381c1d512390b788f1f6b99000b8
tdofat <dofat.tony@gmail.com>
2019-12-13 19:31:28 -0500

    Added Regex for error detection

--- gitCloneMe.sh
+++ gitCloneMe.sh
@@ -4,4 +4,9 @@
 echo Welcome to CloneGitMe
 read -p 'Enter Git SSH Key: ' cloneAdd
 
+if [[ '$cloneAdd' =~ ^[a-z]+@[a-z]+\.[a-z]\:[A-Za-z0-9._]+\/[A-za-z0-9._-]+\.[a-z] ]]
+then
     git clone $cloneAdd
+else
+    echo !!!INVALID SSH KEY!!!
+fi 
